e2781c1196299f9915bd5574fec9ca5152cee517
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-01 19:16:56 +0300

    Implementing : DI between power service and other modules

--- side-project-on-di/cpu/cpu.service.ts
+++ side-project-on-di/cpu/cpu.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { PowerService } from '../power/power.service';
 @Injectable()
-export class CpuService {}
+export class CpuService {
+    constructor(private powerService : PowerService){}
+
+
+    compute(a : number , b : number){
+        console.log(`Drawing 10 watts if power from Power Service`);
+        return a + b ;
+    }
+}
