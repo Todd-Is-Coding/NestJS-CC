e7882f595c374e326905df3b99c0e3b3dbfd80da
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-01 17:12:24 +0300

    Implementing : messages service & repository

--- nest-js-cc/src/messages/messages.controller.ts
+++ nest-js-cc/src/messages/messages.controller.ts
@@ -1,22 +1,23 @@
 import { Controller , Get, Post , Body , Param } from '@nestjs/common';
 import { createMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
 
+    messageServices : MessagesService();
     @Get()
     listMessageS(){
-
+        return this.messageServices.findAll()
     }
 
     @Post()
     createMessage(@Body() body : createMessageDTO){
-        console.log(body);
-        return body;
+        return this.messageServices.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id : string){
-        console.log(id);
+        return this.messageServices.findOne(id);
     }
 
 }
---
+++ nest-js-cc/src/messages/messages.service.ts
@@ -0,0 +1,21 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messageRepo: MessagesRepository;
+
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messageRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messageRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messageRepo.create(content);
+  }
+}
