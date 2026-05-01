35dc7c20b6731fa82c7b00bd85da2bab56873b3c
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-01 18:10:04 +0300

    Refactoring : turning my code to depend on IoC & DI

--- nest-js-cc/src/messages/messages.controller.ts
+++ nest-js-cc/src/messages/messages.controller.ts
@@ -1,23 +1,33 @@
-import { Controller , Get, Post , Body , Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { createMessageDTO } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
+  constructor(public messageServices: MessagesService) {}
+  @Get()
+  listMessageS() {
+    return this.messageServices.findAll();
+  }
 
-    messageServices : MessagesService();
-    @Get()
-    listMessageS(){
-        return this.messageServices.findAll()
+  @Post()
+  createMessage(@Body() body: createMessageDTO) {
+    return this.messageServices.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messageServices.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
     }
-
-    @Post()
-    createMessage(@Body() body : createMessageDTO){
-        return this.messageServices.create(body.content);
-    }
-
-    @Get('/:id')
-    getMessage(@Param('id') id : string){
-        return this.messageServices.findOne(id);
-    }
-
+    return message;
+  }
 }
--- nest-js-cc/src/messages/messages.service.ts
+++ nest-js-cc/src/messages/messages.service.ts
@@ -1,21 +1,19 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messageRepo: MessagesRepository;
-
-  constructor() {
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
-    return this.messageRepo.findOne(id);
+    return this.messagesRepo.findOne(id);
   }
 
   findAll() {
-    return this.messageRepo.findAll();
+    return this.messagesRepo.findAll();
   }
 
   create(content: string) {
-    return this.messageRepo.create(content);
+    return this.messagesRepo.create(content);
   }
 }
